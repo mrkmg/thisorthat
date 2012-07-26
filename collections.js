02341b015044837a5d3ab909c6ecde16149fc230
Kevin Gravier <kgravier@fluctuationsoftware.com>
2012-07-26 12:28:43 -0400

    First Commit

---
+++ collections.js
@@ -0,0 +1,4 @@
+var Polls = new Meteor.Collection('polls');
+var Voters = new Meteor.Collection('voters');
+var Votes = new Meteor.Collection('votes');
+var Trending = new Meteor.Collection('trending');
